044e6437d45ae869af865b8a9322043164794bc3
roei <roeilahaving@gmail.com>
2025-08-20 14:30:35 +0300

    .

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,4 +3,5 @@ export const contactsData = {
     phone: '0526828606',
     github: 'https://github.com/roeilahav',
     linkedIn: 'https://www.linkedin.com/in/roeilahav/',
+    devUsername: "said7388",
 }
